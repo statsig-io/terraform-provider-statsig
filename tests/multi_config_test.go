46a36c921a7ed892c5c9fdf83858cfcdc1bd8742
Kenny Yi <kenny@statsig.com>
2025-02-28 15:30:22 -0800

    Migrate multi config test

---
+++ tests/multi_config_test.go
@@ -0,0 +1,51 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/config"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccMultiConfig(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: protoV6ProviderFactories(),
+		PreCheck:                 func() { testAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				ConfigFile: config.StaticFile("test_resources/multi_config.tf"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("statsig_gate.simple_gate_a", "id", "simple_gate_a"),
+					resource.TestCheckResourceAttr("statsig_gate.simple_gate_b", "id", "simple_gate_b"),
+					resource.TestCheckResourceAttr("statsig_experiment.simple_experiment", "id", "simple_experiment"),
+					verifyMultiConfigSetup(t),
+				),
+			},
+			{
+				ConfigFile:         config.StaticFile("test_resources/multi_config.tf"),
+				ExpectNonEmptyPlan: false,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("statsig_gate.simple_gate_a", "id", "simple_gate_a"),
+					resource.TestCheckResourceAttr("statsig_gate.simple_gate_b", "id", "simple_gate_b"),
+					resource.TestCheckResourceAttr("statsig_experiment.simple_experiment", "id", "simple_experiment"),
+					verifyMultiConfigSetup(t),
+				),
+			},
+		},
+	})
+}
+
+func verifyMultiConfigSetup(t *testing.T) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		root := s.RootModule()
+		gatesOs, _ := root.Outputs["gates"]
+		experimentsOs, _ := root.Outputs["experiments"]
+
+		assert.Equal(t, 2, len(gatesOs.Value.([]interface{})))
+		assert.Equal(t, 1, len(experimentsOs.Value.([]interface{})))
+
+		return nil
+	}
+}
--- tests/test_resources/multi_config.tf
+++ tests/test_resources/multi_config.tf
@@ -3,13 +3,17 @@ resource "statsig_gate" "simple_gate_a" {
   description = "A short description of what this Gate is used for."
   is_enabled  = true
   id_type     = "userID"
-  rules {
-    name            = "All Conditions"
-    pass_percentage = 10
-    conditions {
-      type = "public"
+  rules = [
+    {
+      name            = "All Conditions"
+      pass_percentage = 10
+      conditions = [
+        {
+          type = "public"
+        }
+      ]
     }
-  }
+  ]
 }
 
 resource "statsig_gate" "simple_gate_b" {
@@ -25,19 +29,15 @@ resource "statsig_experiment" "simple_experiment" {
   description = "A short description of what this Experiment is used for."
   id_type     = "userID"
   allocation  = 10
-  groups {
-    name                  = "Test"
-    size                  = 50
-    parameter_values_json = jsonencode({ "a_string" : "test" })
-  }
-  groups {
-    name                  = "Control"
-    size                  = 50
-    parameter_values_json = jsonencode({ "a_string" : "control" })
-  }
-  lifecycle {
-    ignore_changes = ["secondary_metrics_json"] # Automatically attached core tag
-  }
+  groups = [{
+    name             = "Test"
+    size             = 50
+    parameter_values = { a_string = "test" }
+    }, {
+    name             = "Control"
+    size             = 50
+    parameter_values = { a_string = "control" }
+  }]
 }
 
 output "gates" {
