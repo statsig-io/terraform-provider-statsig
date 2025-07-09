a9e82d6a7ecfc8a78368d07a313bf83534d5baee
Daniel West <danielwest@statsig.com>
2025-07-09 16:17:28 -0700

    Settings Roles - docs

---
+++ examples/resources/statsig_settings_roles/resource.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    statsig = {
+      version = "~> 2.0.0"
+      source  = "statsig-io/statsig"
+    }
+  }
+}
+
+resource "statsig_settings_roles" "main" {
+  default_project_role  = "Member"
+}
